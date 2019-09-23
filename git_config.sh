55ec394d470e8a7a381b66196442df33ab4d8472
Frankie Cleary <fcleary@captechconsulting.com>
2019-09-23 13:04:11 -0400

    Simplify githooks

--- git_config.sh
+++ git_config.sh
@@ -7,11 +7,7 @@ git pull upstream master
 
 # create and checkout new branch for student to work on 
 git checkout -b develop
-git push origin develop
 
 # protect against master commits
-[ -d ".git/hooks" ] && exit 0
-mkdir .git/hooks
-mv git-hooks/pre-commit .git/hooks/pre-commit
-chmod +x .git/hooks/pre-commit
-rmdir git-hooks
+chmod +x githooks/pre-commit
+git config core.hooksPath githooks
