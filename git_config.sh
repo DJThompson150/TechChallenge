787fa7e51908dcc24fafce7327aa0006b8027a5e
Frankie Cleary <fcleary@captechconsulting.com>
2019-09-24 12:17:37 -0400

    Allow auto 'no commit' merge and push to origin master

--- git_config.sh
+++ git_config.sh
@@ -3,11 +3,11 @@
 # add the source remote
 git remote add upstream 'https://github.com/captechconsulting/aaml-tc01-mnist.git'
 git fetch upstream master
-git pull upstream master
+git pull upstream --allow-unrelated-histories --no-edit master
+git push origin master
 
 # create and checkout new branch for student to work on 
 git checkout -b develop
-git pull --allow-unrelated-histories
 
 # protect against master commits
 chmod +x githooks/pre-commit
